4e2f074beb26f372cee6bf42994a3b2dd87e5c02
thulasithan-gnanenthiram <thulasithan.gnanenthiram@jahan.ai>
2025-04-18 21:42:48 +0530

    added new function

--- sample/example.py
+++ sample/example.py
@@ -2,5 +2,12 @@
 def find_total(a, b):
     return a + b
 
+def calc_subtract(a, b):
+    return a - b
+
 total = find_total(1, 2)
 print(f"Total: {total}")
+
+print(f"Subtract: {calc_subtract(5, 3)}")
+
+
