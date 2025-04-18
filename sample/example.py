b32a9ae4ae65c97f574465e7acea14a5c6758953
thulasithan-gnanenthiram <thulasithan.gnanenthiram@jahan.ai>
2025-04-18 21:53:53 +0530

    testing

--- sample/example.py
+++ sample/example.py
@@ -8,6 +8,6 @@ def calc_subtract(a, b):
 total = find_total(1, 2)
 print(f"Total: {total}")
 
-print(f"Subtract values awesome: {calc_subtract(5, 3)}")
+print(f"Subtract values main: {calc_subtract(5, 3)}")
 
 
