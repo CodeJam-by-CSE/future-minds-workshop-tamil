b6699815a11efa29b9fae407b2052bf37fd746f4
thulasithan-gnanenthiram <thulasithan.gnanenthiram@jahan.ai>
2025-04-19 18:24:24 +0530

    about to conflict again

--- sample/example.py
+++ sample/example.py
@@ -6,6 +6,6 @@ def subtract_value(a, b):
     return a - b
 
 total = find_total(1, 5)
-print(f"Total from feature-2 branch about to conflict: {total}")
+print(f"Total from feature-2 branch about to conflict again: {total}")
 
 
