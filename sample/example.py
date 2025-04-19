dabc69b186879f261844d23ead58c705c97c37d4
thulasithan-gnanenthiram <thulasithan.gnanenthiram@jahan.ai>
2025-04-19 16:13:41 +0530

    back to init

--- sample/example.py
+++ sample/example.py
@@ -2,14 +2,7 @@
 def find_total(a, b):
     return a + b
 
-def calc_subtract(a, b):
-    return a - b
-
 total = find_total(1, 2)
 print(f"Total: {total}")
-
-print(f"Subtract values main: {calc_subtract(5, 3)}")
-
-print("Testing conflicts from main 2nd time")
 
 
